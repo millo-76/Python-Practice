942fa529381f400fe94ad4e2dadb7b64c1c15f0e
Marlin (ST-OIT11) <st-oit11@uccs.edu>
2025-12-09 10:27:56 -0700

    modified:   INFS 3070/Assignments/MarlinJaramilloProject2.ipynb 	new file:   INFS 3070/Assignments/__pycache__/dbConfig.cpython-312.pyc 	new file:   INFS 3070/Assignments/dbConfig.py

---
+++ INFS 3070/Assignments/dbConfig.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+
+def get_conn():
+
+    return {
+        'username': 'infscompany',
+        'password': 'yeadata', 
+        'host': '128.198.162.191',
+        'database': 'company'
+    }
